364030981320ed34ded1fd44fb889be9ae20640a
Jinam Shah <jinams47@gmail.com>
2024-11-08 13:28:17 -0500

    Added logging

--- src/predict.py
+++ src/predict.py
@@ -12,20 +12,23 @@ log = logging.getLogger(__name__)
 
 
 class ImageBatchProcessor:
-    def __init__(self, image_folder, batch_prefix, start_date, end_date):
+    def __init__(self, image_folder, batch_prefix, start_date, end_date, sample_size):
         self.image_folder = Path(image_folder)
         self.batch_prefix = batch_prefix
         self.start_date = pd.to_datetime(start_date)
         self.end_date = pd.to_datetime(end_date)
         self.df = None
+        self.sample_size = sample_size
 
-    def load_batches(self, sample_size=10):
-        # batches = [x for x in self.image_folder.glob("*") if self.batch_prefix in x.stem]
-        batches = [x for x in self.image_folder.glob('*') if self.batch_prefix in x.stem and (self.start_date <= pd.to_datetime(x.stem.split('_')[1]) <= self.end_date)]
-        if sample_size >= len(batches):
+    def load_batches(self):
+        log.info("Loading batches")
+        batches = [x for x in self.image_folder.glob('*') 
+                   if self.batch_prefix in x.stem and 
+                   (self.start_date <= pd.to_datetime(x.stem.split('_')[1]) <= self.end_date)]
+        if self.sample_size >= len(batches):
             sample_batches = batches
         else:
-            sample_batches = random.sample(batches, sample_size)
+            sample_batches = random.sample(batches, self.sample_size)
         
         dfs = []
         
@@ -34,13 +37,9 @@ class ImageBatchProcessor:
             dfs.append(df)
         
         self.df = pd.concat(dfs, ignore_index=True)
-        self.filter_data()
-
-    def filter_data(self):
-        self.df["batch_date"] = pd.to_datetime(self.df["batch_id"].str.extract(r'(\d{4}-\d{2}-\d{2})')[0])
-        self.df = self.df[(self.df["batch_date"] >= self.start_date) & (self.df["batch_date"] <= self.end_date)]
         self.df = self.df[self.df["common_name"] != "unknown"]
-
+        log.info(f"Working with {len(self.df)} images")
+        
     def get_data(self):
         return self.df
 
@@ -63,7 +62,7 @@ class WeedPredictor:
                 yield images[i:i+batch_size]
 
         def yield_predictions(batch, batch_predictions):
-            for img_path, prediction in zip(batch, batch_predictions):
+            for _, prediction in zip(batch, batch_predictions):
                 yield prediction.probs.top1, prediction.probs.top1conf.cpu().item()
         
         results = []
@@ -72,6 +71,7 @@ class WeedPredictor:
             
             results.extend(yield_predictions(batch, batch_predictions))
             # results.extend(self.model.predict(batch))
+        log.info("Predictions generated (batched mode)")
         return results
 
 class PredictionSaver:
@@ -94,8 +94,8 @@ class PredictionSaver:
 
 
 class BatchInferencePipeline:
-    def __init__(self, image_folder, batch_prefix, model_path, label_map, output_folder, date_range, parallel_processing, batch_size):
-        self.processor = ImageBatchProcessor(image_folder, batch_prefix, *date_range)
+    def __init__(self, image_folder, batch_prefix, model_path, label_map, output_folder, date_range, parallel_processing, batch_size, sample_size):
+        self.processor = ImageBatchProcessor(image_folder, batch_prefix, *date_range, sample_size)
         self.predictor = WeedPredictor(model_path, label_map)
         self.saver = PredictionSaver(output_folder)
         self.predicted_rows = []
@@ -108,10 +108,13 @@ class BatchInferencePipeline:
         
 
         if self.parallel_processing:
+            log.info(f"Generating labels in parallel with batch size of {self.batch_size}, resulting in approximately { int(len(df)/self.batch_size) } batches")
             images = []
             df['img_path'] = df.apply(lambda row: self.processor.image_folder / row['batch_id'] / f"{row['cutout_id']}.jpg", axis=1)
             images = df['img_path'].tolist()
             results = self.predictor.batch_predict(images, self.batch_size)
+
+            log.info("Saving predictions and diving images based on confidence scores (batched mode)")
             for record, result in zip(df.iterrows(), results):
                 row = record[1]
                 target_class, confidence = result
@@ -125,6 +128,7 @@ class BatchInferencePipeline:
                     
                 self.predicted_rows.append(row)
         else:
+            log.info("Generating predictions (serial mode)")
             for _, row in df.iterrows():
                 img_name = f"{row['cutout_id']}.jpg"
                 batch_id = row["batch_id"]
@@ -142,14 +146,18 @@ class BatchInferencePipeline:
                 
                 self.predicted_rows.append(row)
         
-        self.save_results()
+        result_csv = self.save_results()
+        return result_csv
 
     def save_results(self):
         timestamp = pd.Timestamp.now().strftime("%Y%m%d_%H%M%S")
         output_filename = self.saver.output_folder / f"prediction_batches_{timestamp}.csv"
         pd.DataFrame(self.predicted_rows).to_csv(output_filename, index=False)
+        return output_filename
 
 def main(cfg):
+    start_time = datetime.now()
+
     process_config = cfg['predict']
     pipeline = BatchInferencePipeline(
         image_folder=process_config['image_folder'],
@@ -159,10 +167,12 @@ def main(cfg):
         output_folder=process_config['output_folder'],
         date_range=(process_config['start_date'], process_config['end_date']),
         parallel_processing=process_config['parallel_inference'],
-        batch_size=process_config['inference_batch_size']
+        batch_size=process_config['inference_batch_size'],
+        sample_size=int(process_config['sample_folder_count'])
     )
-
-    pipeline.run()
+    results = pipeline.run()
+    log.info(f"Processing complete. Time spent: {datetime.now() - start_time}")
+    log.info(f"Labels generated and saved to {results}")
 
 
 # Usage example
