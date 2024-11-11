17bba6c2a3726dfb5216d6558dd71899ad1c45a1
Matthew Kutugata <mattkutugata@gmail.com>
2024-11-11 13:30:17 -0500

    starter code for labelers and config

--- src/image_curation_labeler.py
+++ src/image_curation_labeler.py
@@ -7,34 +7,6 @@ import numpy as np
 from tqdm import tqdm
 import random
 
-def df_filter(df, image_folder):
-    # Get list of images
-    images = [Path(img).name for img in os.listdir(image_folder) if img.endswith(".jpg")]
-    df = df[df["Name"].isin(images)]
-    return df
-
-def stratified_sample(df: pd.DataFrame, n_samples_per_bin=5, max_bins=10):
-    """
-    Generate an even sample across the smallest area bins for each species.
-    
-    Parameters:
-        df (pd.DataFrame): DataFrame containing the data to sample from.
-        n_samples_per_bin (int): Number of samples to take per bin for each species.
-        max_bins (int): Number of the smallest bins to retain for sampling.
-    """
-    # Define logarithmic bins for the area
-    bins = np.logspace(np.log10(df["area"].min() + 1), np.log10(df["area"].max() + 1), num=10)
-    df["area_bin"] = pd.cut(df["area"], bins=bins, labels=range(len(bins) - 1), duplicates="drop")
-    
-    # Select only the smallest bins
-    smallest_bins = range(max_bins)  # Select the first few (smallest) bins
-    df = df[df["area_bin"].isin(smallest_bins)]
-    
-    # Perform stratified sampling across the filtered bins
-    sampled_df = df.groupby(["common_name", "area_bin"], group_keys=False).apply(
-        lambda x: x.sample(min(len(x), n_samples_per_bin))
-    )
-    return sampled_df
 
 def get_dynamic_font_scale(image_height, image_width):
     """Dynamically adjust the font scale based on image size."""
@@ -136,52 +108,26 @@ def image_viewer(df: pd.DataFrame, image_folder, output_folder):
     # Save results to CSV
     results.to_csv(output_csv, index=False)
 
-def filter_by_season(df, keyword):
-    """Filter rows where 'Seasons' column contains the word 'cover'."""
-    return df[df["season"].str.contains(keyword, case=False, na=False)]
 
 
 if __name__ == "__main__":
-    # Example usage
-    batch_prefix = "MD"
-
-    image_folder = Path("/mnt/research-projects/s/screberg/longterm_images/semifield-cutouts")
-    # image_folder = Path("/mnt/research-projects/s/screberg/GROW_DATA/semifield-cutouts")
     
-
-    batches = [x for x in image_folder.glob("*") if batch_prefix in x.name]
+    data_folder = Path("../predictions_test")
     
-    print(f"Total number of batches before date filtering: {len(batches)}")
-    start_date = pd.to_datetime("2022-10-12")
-    end_date = pd.to_datetime("2023-05-20")
     
-    # Filter batches by date
-    filtered_batches = []
-    for batch in batches:
-        date_str = batch.name.split("_")[1]
-        batch_date = pd.to_datetime(date_str)
-        if start_date <= batch_date <= end_date:
-            filtered_batches.append(batch)
-    
-    print(f"Total number of batches after date filtering: {len(filtered_batches)}")
+    image_folder = Path(data_folder, "non_target_class_35_50")
+   
 
-    batch_sample = random.sample(filtered_batches, 10 if len(filtered_batches) > 10 else len(filtered_batches))
-
-    # csvs = []
-    # for batch in tqdm(batch_sample):
-    #     csv = [x for x in batch.glob("*.csv")][0]
-    #     csvs.append(csv)
- 
-    data_folder = Path("../labels/md_covers")
     csvs = [x for x in data_folder.rglob("*.csv")]
     cutout_ids = [x.stem for x in image_folder.glob("*.jpg")]
     df = pd.read_csv([x for x in data_folder.glob("*.csv")][0])
     df = df[df["cutout_id"].isin(cutout_ids)]
 
-    
+    # df = pd.concat([pd.read_csv(csv) for csv in csvs], ignore_index=True)
     print(f"Size of df before filtering: {len(df)}")
     
-    data_folder.mkdir(exist_ok=True, parents=True)
+    output_folder = Path("../labels/md_covers")
+    output_folder.mkdir(exist_ok=True, parents=True)
 
     # Load all CSV files into a single DataFrame
     output_csvs = [x for x in output_folder.glob("*.csv")]
@@ -191,12 +137,5 @@ if __name__ == "__main__":
         labeled_df = pd.concat([pd.read_csv(csv) for csv in output_csvs], ignore_index=True)
         df = df[~df["cutout_id"].isin(labeled_df["cutout_id"])]
 
-    df = filter_by_season(df, "cover")
-    print(f"Size of df before filtering: {len(df)}")
-    df = stratified_sample(df, n_samples_per_bin=20, max_bins=6)
-    if len(df) == 0:
-        print("No images to process")
-        exit()
-    
     print(f"Processing {len(df)} images")
     image_viewer(df, image_folder, output_folder)
